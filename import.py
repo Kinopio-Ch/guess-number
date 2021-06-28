ebb7300f60798affc93ab260ac55a0bea5ecd56f
Kinopio <madeforthenetflix@gmai.com>
2021-06-28 16:35:35 +0800

    basic_version

---
+++ import.py
@@ -0,0 +1,20 @@
+#猜數字遊戲
+
+import random
+
+r = random.randint(1, 100)
+while True:
+    num = input('請猜數字: ')
+    num = int(num)
+    if num == r:
+        print('猜對喔~')
+        break
+    elif num > r:
+        print('比答案大')
+    elif num < r:
+        print('比答案小')
+
+
+
+
+
