c81a61eb34068427bc03360de80a76378e71261c
Kinopio <madeforthenetflix@gmai.com>
2021-06-28 17:19:36 +0800

    user decide range

--- import.py
+++ import.py
@@ -1,10 +1,16 @@
 #猜數字遊戲
 
 import random
+start = input('請決定答案最小值: ') #使用者決定最大最小值
+end = input('請決定答案最大值: ')
+start = int(start)
+end = int(end)
+
 
 ##################################################################
 
-r = random.randint(1, 100)
+
+r = random.randint(start, end)
 count = 0   #猜測次數計算，寫在while迴圈外以免歸零
 
 while True:
